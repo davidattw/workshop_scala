be7cc828b4201dc00b5f5e04580250c7e8fe82c8
newlee <newlee.cn@gmail.com>
2014-06-14 09:35:48 +0800

    refactor test

--- tdd/guess_number/src/test/scala/workshop/tdd/GuessNumberSpec.scala
+++ tdd/guess_number/src/test/scala/workshop/tdd/GuessNumberSpec.scala
@@ -2,20 +2,26 @@ package workshop.tdd
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.{Matchers, FunSpec}
+import org.scalatest.{BeforeAndAfterEach, Matchers, FunSpec}
 
 @RunWith(classOf[JUnitRunner])
-class GuessNumberSpec extends FunSpec with Matchers{
+class GuessNumberSpec extends FunSpec with Matchers with BeforeAndAfterEach{
+  var game: Game = null
+
+  override protected def beforeEach(): Unit = {
+    game = Game("1 2 3 4").start()
+  }
+
   describe("guess number"){
     it("should return 0A0B when no number is correct") {
-      val game: Game = Game("1 2 3 4").start()
-      val result: String = game.guess("5 6 7 8")
+      val guessNumber = "5 6 7 8"
+      val result = game.guess(guessNumber)
       result should be("0A0B")
     }
 
     it("should return 4A0B when all number is correct") {
-      val game: Game = Game("1 2 3 4").start()
-      val result: String = game.guess("1 2 3 4")
+      val guessNumber = "1 2 3 4"
+      val result = game.guess(guessNumber)
       result should be("4A0B")
     }
 
