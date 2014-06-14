c5426c51c912c655b04e3091997bbe10189b0047
newlee <newlee.cn@gmail.com>
2014-06-14 09:57:48 +0800

    refactor, make code more simple

--- tdd/guess_number/src/main/scala/workshop/tdd/Answer.scala
+++ tdd/guess_number/src/main/scala/workshop/tdd/Answer.scala
@@ -8,12 +8,8 @@ object Answer{
 class Answer(private val numbers: Array[Int]) {
   def compare(other: Answer): String = {
     val numberCorrectCount = numbers.intersect(other.numbers).size
-    val bullsCount = numberWithIndex(numbers).intersect(numberWithIndex(other.numbers)).length
+    val bullsCount = numbers.zip(other.numbers).count(p => p._1 == p._2)
     val cowsCount = numberCorrectCount - bullsCount
     f"$bullsCount%sA$cowsCount%sB"
-  }
-
-  private def numberWithIndex(numbers: Array[Int]): IndexedSeq[(Int, Int)] = {
-    (0 to 3).zip(numbers)
   }
 }
